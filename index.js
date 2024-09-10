cb2d2ac7472f1da7021eab3e041c401fe01e3f65
Muhammet Ali Büyük <143749828+qanteSm@users.noreply.github.com>
2024-09-10 19:40:38 +0300

    Update index.js
    php sayfasında silinen mesajlar görünür hale getirildi

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 const fs = require("fs");
@@ -5,7 +6,7 @@ const path = require("path");
 const checkDiskSpace = require("check-disk-space").default;
 const { exec } = require("child_process");
 const util = require("util");
-const { myphone } = require("./config.js")
+const { myphone } = require("./config.js");
 const execPromise = util.promisify(exec);
 
 const client = new Client({
@@ -17,10 +18,48 @@ const client = new Client({
   },
 });
 
+const deletedMessagesCache = new Map();
+const MAX_CACHE_SIZE = 1000;
+
+function generateMessageKey(message) {
+  const timestamp = message.timestamp; 
+  const sender = message.from;
+  return `${timestamp}-${sender}`;
+}
+
 client.on("ready", () => {
   console.log("Client is ready!");
 });
+function parseMessage(message) {
+    if (!message || message.trim() === "") {
+      return { command: null, args: [] };
+    }
+  
+    const parts = message.trim().split(/\s+/);
+  
+    const command = parts[0].startsWith("!") ? parts[0].slice(1).toLowerCase() : parts[0].toLowerCase();
+  
+    const args = parts.slice(1);
+  
+    return { command, args };
+  }
+  function parseMessage2(message) {
+  if (!message || message.trim() === "") {
+    return { command: null, args: "" };
+  }
 
+  const commandStartIndex = message.indexOf("!") + 1;
+
+  const commandEndIndex = message.indexOf(" ", commandStartIndex);
+
+  const command = commandEndIndex === -1 
+    ? message.slice(commandStartIndex).toLowerCase() 
+    : message.slice(commandStartIndex, commandEndIndex).toLowerCase();
+
+  const args = message.slice(commandEndIndex + 1).trim();
+
+  return { command, args };
+}
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 });
@@ -28,36 +67,36 @@ client.on("qr", (qr) => {
 client.initialize();
 
 client.on("message_create", async (message) => {
+	const senderNumber = message.from;
+	const contact = await message.getContact();
+	const senderName = contact.name || senderNumber; 
+	console.log(senderNumber);
   try {
-    if( ""+message.from == myphone+"@c.us" && ""+message.to == myphone+"@c.us"){
-      console.log(message)
-      if(message.body === "!depolama"){
-
+    if ("" + message.from == myphone + "@c.us" && "" + message.to == myphone + "@c.us") {
+      console.log(message);
+      if (message.body === "!depolama") {
         const storageReport = await generateStorageReport();
         await message.reply(storageReport);
-        return
-      } else if(message.body.startsWith("!mesajlar")) {
+        return;
+      } else if (message.body.startsWith("!mesajlar")) {
         const args = message.body.slice("!mesajlar".length).trim().split(/ +/);
-        const numara = args[0]
-        const gün = args[1]
-        if(!numara || !gün){
-          message.reply("Girilen verielr yanlış! syntax: !mesajlar (numara) (gün: 2025-07-07)")
+        const numara = args[0];
+        const gün = args[1];
+        if (!numara || !gün) {
+          message.reply("Girilen veriler yanlış! syntax: !mesajlar (numara) (gün: 2025-07-07)");
           return;
         }
         const whatsappMessagesDir = "whatsapp_messages";
         const messagesFile = "messages.json";
-        const numarapath = path.join(whatsappMessagesDir, numara+"@c.us");
+        const numarapath = path.join(whatsappMessagesDir, numara + "@c.us");
         const tarihpath = path.join(numarapath, gün);
         const messagesFilePath = path.join(tarihpath, messagesFile);
         if (!fs.existsSync(numarapath) || !fs.existsSync(tarihpath)) {
           message.reply("Klasörler bulunamadı.");
-          return; 
+          return;
         }
-         const messagesData = JSON.parse(fs.readFileSync(messagesFilePath, 'utf-8'));
         try {
-          const messagesData = JSON.parse(
-            fs.readFileSync(messagesFilePath, "utf-8")
-          );
+          const messagesData = JSON.parse(fs.readFileSync(messagesFilePath, "utf-8"));
 
           const messagesText = JSON.stringify(messagesData, null, 2);
 
@@ -75,151 +114,272 @@ client.on("message_create", async (message) => {
                 console.log("Dosya başarıyla silindi!");
               }
             });
-          })
-          
+          });
+
           console.log(txtFilePath);
         } catch (err) {
-          message.reply("Bİrader json gönderilirken bi sorun oluştu :(")
+          message.reply("Bir sorun oluştu :(");
           console.error("Hata oluştu:", err);
         }
-
       }
       return;
     }
-    console.log(myphone + "@c.us");
-    console.log(message.from);
-    console.log(message);
-    const sender = message.from;
-    const isGroup = message.isGroupMsg;
-
-    let author;
-    if (isGroup) {
-      try {
-        const contact = await message.getContact();
-        author = contact.id.user;
-      } catch (error) {
-        console.error("Gönderen kişi bilgisi alınamadı:", error);
-        author = undefined;
-      }
-    } else {
-      author = sender;
-    }
-    const today2 = new Date();
-    const options = { year: "numeric", month: "numeric", day: "numeric" };
-    const formattedDate = new Intl.DateTimeFormat("tr-TR", options).format(
-      today2
-    );
-
-    const parts = formattedDate.split("."); 
-    const [day, month, year] = parts;
-    const formattedDate2 = `${year}-${month}-${day}`;
-    const today = formattedDate2;
-    console.log(today);
-    const baseDir = path.join("whatsapp_messages", sender);
-    const todayDir = path.join(baseDir, today);
-    const srcDir = path.join(todayDir, "src");
-    const messagesFile = path.join(todayDir, "messages.json");
-
-    fs.mkdirSync(baseDir, { recursive: true });
-    fs.mkdirSync(todayDir, { recursive: true });
-    fs.mkdirSync(srcDir, { recursive: true });
-
-    let messages = [];
-    try {
-      messages = JSON.parse(fs.readFileSync(messagesFile, "utf-8"));
-    } catch (err) {}
-
-    const timestamp = new Date(message.timestamp * 1000);
-    const formattedTimestamp = timestamp.toLocaleString();
-    let newMessage;
-    console.log(message._data.notifyName);
-    if (message.pollOptions){
-      newMessage = {
-        timestamp: message.timestamp + " (" + formattedTimestamp + ")",
-        messageid: message.id.id,
-        realaythor: message.author,
-        name: message._data.notifyName || "yokki",
-        author,
-        body: message.body,
-        secenekler: message.pollOptions,
-      };
-    }else {
-      newMessage = {
-        timestamp: message.timestamp + " (" + formattedTimestamp + ")",
-        messageid: message.id.id,
-        realaythor: message.author,
-        name: message._data.notifyName || "yokki",
-        author,
-        body: message.body,
-      };
-    }
-    
+	
+    let messageData = { ...message._data };
 
     if (message.hasMedia) {
       const media = await message.downloadMedia();
-      if (media) {
-        let extension = media.mimetype.split("/")[1];
-        if (extension === "x-rar-compressed") {
-          extension = "rar";
-        }
+      if (media) {	
+        const extension = media.mimetype.split("/")[1]; 
+
         let filename = `${message.timestamp}.${extension}`;
 
-        const filePath = path.join(srcDir, filename);
+        const sender = message.from;
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = ("0" + (today.getMonth() + 1)).slice(-2);
+        const day = ("0" + today.getDate()).slice(-2);
+        const formattedDate = `${year}-${month}-${day}`;
+
+        const baseDir = path.join("whatsapp_messages", sender);
+        const todayDir = path.join(baseDir, formattedDate);
+        const srcDir = path.join(todayDir, "src");
+
+        fs.mkdirSync(baseDir, { recursive: true });
+        fs.mkdirSync(todayDir, { recursive: true });
+        fs.mkdirSync(srcDir, { recursive: true });
+
+        const filePath = path.join(srcDir, filename); 
+        
         fs.writeFileSync(filePath, media.data, { encoding: "base64" });
 
-        if (media.mimetype === "audio/ogg; codecs=opus") {
-          const mp3Filename = `${message.timestamp}.mp3`;
-          const mp3FilePath = path.join(srcDir, mp3Filename);
-
-          execPromise(`ffmpeg -i "${filePath}" "${mp3FilePath}"`)
-            .then(() => {
-              fs.unlinkSync(filePath);
-
-              newMessage.media = {
-                type: "audio/mpeg",
-                filename: mp3Filename,
-              };
-              console.log("tureamii");
-
-              messages.push(newMessage);
-              console.log("santamaria");
-              fs.writeFileSync(messagesFile, JSON.stringify(messages, null, 2));
-            })
-            .catch((error) => {
-              console.error("ffmpeg conversion error:", error);
-            });
-        } else if (fs.existsSync(filePath) && fs.statSync(filePath).size > 0) {
-          newMessage.media = {
-            type: media.mimetype,
-            filename,
-          };
-          messages.push(newMessage);
-          console.log("santamaria");
-          fs.writeFileSync(messagesFile, JSON.stringify(messages, null, 2));
-        } else {
-          console.error(
-            "Media file could not be downloaded or saved:",
-            filename
-          );
-        }
+        messageData.media = {
+          type: media.mimetype,
+          filename,
+          path: filePath, 
+        };
       }
-    } else {
-      messages.push(newMessage);
-      console.log("santamaria");
-      fs.writeFileSync(messagesFile, JSON.stringify(messages, null, 2));
     }
+
+    const messageKey = generateMessageKey(message);
+    deletedMessagesCache.set(messageKey, messageData);
+    if (deletedMessagesCache.size > MAX_CACHE_SIZE) {
+      deletedMessagesCache.delete(deletedMessagesCache.keys().next().value);
+    }
+
+    await saveMessage(message);
   } catch (err) {
     console.log(err);
   }
 });
 
-
 client.on("message_revoke_everyone", async (message) => {
-  
+  try {
+    const approximateTimestamp = message.timestamp;
+    const sender = message.from;
+
+    const candidateMessages = Array.from(deletedMessagesCache.entries())
+      .filter(([key, msg]) => {
+        const messageTimestamp = parseInt(key.split('-')[0]);
+        return Math.abs(messageTimestamp - approximateTimestamp) <= 5;
+      })
+      .map(([key, msg]) => msg);
+
+    const deletedMessage = candidateMessages.find(msg => msg.from === sender);
+
+    if (deletedMessage) {
+      console.log("Silinen Mesaj:", deletedMessage.body);
+      if (deletedMessage.media) {
+        console.log("Silinen Mesaj Medyası:", deletedMessage.media);
+
+        const oggFilename = deletedMessage.media.filename;
+        const oggFilePath = deletedMessage.media.path;
+
+        const mp3Filename = oggFilename.replace('.ogg; codecs=opus', '.mp3');
+        const mp3FilePath = oggFilePath.replace('.ogg; codecs=opus', '.mp3');
+
+        const deletedMediaPath = "C:/xampp/htdocs/deleted_media";
+        fs.mkdirSync(deletedMediaPath, { recursive: true });
+        const newFilePath = path.join(deletedMediaPath, mp3Filename);
+
+        if (fs.existsSync(mp3FilePath)) {
+          fs.renameSync(mp3FilePath, newFilePath);
+        } else {
+          console.error("Taşınacak .mp3 dosyası bulunamadı:", mp3FilePath);
+        }
+
+        deletedMessage.media.path = newFilePath;
+        deletedMessage.media.filename = mp3Filename;
+        deletedMessage.media.type = "audio/mpeg";
+      }
+
+      await saveDeletedMessage(deletedMessage);
+
+      const messageKey = generateMessageKey(deletedMessage);
+      deletedMessagesCache.delete(messageKey);
+    } else {
+      console.log("Mesaj hafızada bulunamadı. Eski bir mesaj olabilir.");
+    }
+  } catch (err) {
+    console.error("Mesaj silme işlemi sırasında hata oluştu:", err);
+  }
 });
+async function saveMessage(message) {
+  const sender = message.from;
+  const isGroup = message.isGroupMsg;
 
+  let author;
+  if (isGroup) {
+    try {
+      const contact = await message.getContact();
+      author = contact.id.user;
+    } catch (error) {
+      console.error("Gönderen kişi bilgisi alınamadı:", error);
+      author = undefined;
+    }
+  } else {
+    author = sender;
+  }
 
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = ("0" + (today.getMonth() + 1)).slice(-2);
+  const day = ("0" + today.getDate()).slice(-2);
+  const formattedDate = `${year}-${month}-${day}`;
+  console.log("sa");
+  let baseDir;
+  if (sender == myphone+"@c.us"){
+    baseDir = path.join("whatsapp_messages", message.to);
+    console.log(`1message to: ${message.to} message from: ${message.from}`)
+  }else {
+	  console.log(`2message to: ${message.to} message from: ${message.from}`)
+    baseDir = path.join("whatsapp_messages", sender);
+  }
+  
+  const todayDir = path.join(baseDir, formattedDate);
+  const srcDir = path.join(todayDir, "src");
+  const messagesFile = path.join(todayDir, "messages.json");
 
+  fs.mkdirSync(baseDir, { recursive: true });
+  fs.mkdirSync(todayDir, { recursive: true });
+  fs.mkdirSync(srcDir, { recursive: true });
+
+  let messages = [];
+  try {
+    messages = JSON.parse(fs.readFileSync(messagesFile, "utf-8"));
+  } catch (err) {}
+
+  const timestamp = new Date(message.timestamp * 1000);
+  const formattedTimestamp = timestamp.toLocaleString();
+  let newMessage;
+  console.log(message._data.notifyName);
+  if (message.pollOptions) {
+    newMessage = {
+      timestamp: message.timestamp + " (" + formattedTimestamp + ")",
+      messageid: message.id.id,
+      realaythor: message.author,
+      name: message._data.notifyName || "yokki",
+      author,
+      body: message.body,
+      secenekler: message.pollOptions,
+    };
+  } else {
+    newMessage = {
+      timestamp: message.timestamp + " (" + formattedTimestamp + ")",
+      messageid: message.id.id,
+      realaythor: message.author,
+      name: message._data.notifyName || "yokki",
+      author,
+      body: message.body,
+    };
+  }
+
+  if (message.hasMedia) {
+    const media = await message.downloadMedia();
+    if (media) {
+      if (media.mimetype === "audio/ogg; codecs=opus") {
+        const mp3Filename = `${message.timestamp}.mp3`;
+        const mp3FilePath = path.join(srcDir, mp3Filename);
+
+        try {
+          const tempOggFilename = `${message.timestamp}_temp.ogg`;
+          const tempOggFilePath = path.join(srcDir, tempOggFilename);
+          fs.writeFileSync(tempOggFilePath, media.data, { encoding: "base64" });
+
+          await execPromise(`ffmpeg -i "${tempOggFilePath}" "${mp3FilePath}"`);
+
+          fs.unlinkSync(tempOggFilePath);
+
+          const mp3Data = fs.readFileSync(mp3FilePath, { encoding: 'base64' });
+
+          newMessage.media = {
+            type: "audio/mpeg",
+            filename: mp3Filename,
+            path: mp3FilePath,
+            data: mp3Data 
+          };
+
+          messages.push(newMessage);
+          fs.writeFileSync(messagesFile, JSON.stringify(messages, null, 2));
+        } catch (error) {
+          console.error("ffmpeg dönüştürme hatası:", error);
+        }
+      } else {
+        const extension = media.mimetype.split("/")[1]; 
+        let filename = `${message.timestamp}.${extension}`;
+        const filePath = path.join(srcDir, filename);
+        fs.writeFileSync(filePath, media.data, { encoding: "base64" });
+
+        if (fs.existsSync(filePath) && fs.statSync(filePath).size > 0) {
+          newMessage.media = {
+            type: media.mimetype,
+            filename,
+            path: filePath 
+          };
+          messages.push(newMessage);
+          fs.writeFileSync(messagesFile, JSON.stringify(messages, null, 2));
+        } else {
+          console.error("Medya dosyası indirilemedi veya kaydedilemedi:", filename);
+        }
+      }
+    }
+  } else {
+    messages.push(newMessage);
+    fs.writeFileSync(messagesFile, JSON.stringify(messages, null, 2));
+  }
+}
+
+async function getDeletedMessageInfo(message) {
+  const sender = message.from;
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = ("0" + (today.getMonth() + 1)).slice(-2);
+  const day = ("0" + today.getDate()).slice(-2);
+  const formattedDate = `${year}-${month}-${day}`;
+
+  const messagesFile = path.join("whatsapp_messages", sender, formattedDate, "messages.json");
+
+  if (fs.existsSync(messagesFile)) {
+    const messagesData = JSON.parse(fs.readFileSync(messagesFile, "utf-8"));
+    const deletedMessage = messagesData.find(msg => msg.messageid === message.id.id);
+    return deletedMessage;
+  }
+
+  return null;
+}
+
+async function saveDeletedMessage(deletedMessage) {
+  const deletedMessagesFile = path.join("whatsapp_messages", "deleted_messages.json");
+
+  let deletedMessages = [];
+  try {
+    deletedMessages = JSON.parse(fs.readFileSync(deletedMessagesFile, "utf-8"));
+  } catch (err) {}
+
+  deletedMessages.push(deletedMessage);
+  fs.writeFileSync(deletedMessagesFile, JSON.stringify(deletedMessages, null, 2));
+  console.log("Silinen mesaj deleted_messages.json dosyasına kaydedildi.");
+}
 
 async function generateStorageReport() {
   const baseDir = path.join("whatsapp_messages");
@@ -236,7 +396,7 @@ async function generateStorageReport() {
 
   const folderSizeInBytes = getFolderSize(baseDir);
   const folderSizeString =
-    folderSizeInBytes >= 1024 * 1024 * 1024 
+    folderSizeInBytes >= 1024 * 1024 * 1024
       ? `${(folderSizeInBytes / (1024 * 1024 * 1024)).toFixed(2)} GB`
       : `${(folderSizeInBytes / (1024 * 1024)).toFixed(2)} MB`;
 
@@ -244,17 +404,13 @@ async function generateStorageReport() {
 Depolama Kullanım Raporu:
 
 Toplam Alan: ${usedSpaceInGB} GB
-Kullanılan Alan: ${(
-    diskSpace.size / (1024 * 1024 * 1024) -
-    freeSpaceInGB
-  ).toFixed(2)} GB
+Kullanılan Alan: ${(diskSpace.size / (1024 * 1024 * 1024) - freeSpaceInGB).toFixed(2)} GB
 Kalan Alan: ${freeSpaceString}
 WhatsApp Arşiv Boyutu: ${folderSizeString}
     `;
 
   return report;
 }
-
 
 function getFolderSize(dirPath) {
   let totalSize = 0;
